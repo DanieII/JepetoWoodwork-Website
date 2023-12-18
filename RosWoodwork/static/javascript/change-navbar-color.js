9e3c8c6403fc268b830ba887ccd6a129026134b9
DanieII <danielbogdanov1748@gmail.com>
2023-12-18 23:42:38 +0200

    Changed the website name and started implementing a new design

---
+++ RosWoodwork/static/javascript/change-navbar-color.js
@@ -0,0 +1,21 @@
+const navBar = document.querySelector(".main-nav");
+const navBarHeight = navBar.clientHeight;
+const [whiteLogo, darkLogo] = document.querySelectorAll(".logo-container img");
+
+navBar.classList.add("transparent");
+
+document.addEventListener("scroll", handleScroll);
+
+function handleScroll() {
+  const yPosition = window.scrollY;
+
+  if (yPosition > navBarHeight) {
+    navBar.classList.remove("transparent");
+    whiteLogo.style.display = "block";
+    darkLogo.style.display = "none";
+  } else {
+    navBar.classList.add("transparent");
+    whiteLogo.style.display = "none";
+    darkLogo.style.display = "block";
+  }
+}
