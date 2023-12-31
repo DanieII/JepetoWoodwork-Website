a9d4c8ab381ad5d2d6febc82e36b3a10724d24df
Daniel Bogdanov <danielbogdanov1748@gmail.com>
2023-12-31 18:22:54 +0200

    Finished styling the pages and started implementing caching

--- RoccoWoodwork/products/signals.py
+++ RoccoWoodwork/products/signals.py
@@ -4,8 +4,12 @@ from django.core.cache import cache
 from .models import Product, ProductImage, Category
 
 
-@receiver(post_save, sender=Product)
-@receiver(post_save, sender=ProductImage)
 @receiver(post_save, sender=Category)
-def invalidate_cache(sender, instance, **kwargs):
-    cache.clear()
+def invalidate_categories_cache(sender, instance, **kwargs):
+    cache.delete("categories")
+
+
+# @receiver(post_save, sender=Product)
+# @receiver(post_save, sender=ProductImage)
+# def invalidate_cache(sender, instance, **kwargs):
+#     cache.clear()
